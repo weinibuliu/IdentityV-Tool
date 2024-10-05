ac5364bf875d2fb1b33e503f1d65591bb2fe89d3
Weinibuliu <weinibuliu@outlook.com>
2024-10-05 15:24:58 +0800

    更新

--- install/deps/infos/common.py
+++ install/deps/infos/common.py
@@ -5,20 +5,20 @@ from maa.custom_action import CustomAction
 
 class Move(CustomAction):
     def run(self, context: Context, argv: CustomAction.RunArg) -> CustomAction.RunResult | bool:
-        def move(direction:int=0,duration:int=10):
+        def move(direction:int=0):
             match direction:
                 case 0: #向前移动
-                    context.override_pipeline({"基础移动":{"end": [175,415,10,10], "duration": duration}})
+                    context.run_pipeline("移动_向前")
                 case 1: #向右移动
-                    context.override_pipeline({"基础移动":{"end": [275,515,10,10], "duration": duration}})
+                    context.run_pipeline("移动_向右")
                 case 2: #向后移动
-                    context.override_pipeline({"基础移动":{"end": [175,615,10,10], "duration": duration}})
+                    context.run_pipeline("移动_向后")
                 case 3: #向左移动
-                    context.override_pipeline({"基础移动":{"end": [75,515,10,10], "duration": duration}})
+                    context.run_pipeline("移动_向左")
                 case _:
                     raise (f"Class Error:{__class__.__name__},please contact to the developers.")
-        direction = randint(0,3)   
-        move(direction,10)
-        context.run_pipeline("基础移动")
+                
+        direction = randint(0,3)
+        move(direction)
 
         return True
