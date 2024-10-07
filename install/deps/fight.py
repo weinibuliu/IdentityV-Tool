4ab5d08c23ddcabc8c35daea1b57d9fd3bce77fc
Weinibuliu <weinibuliu@outlook.com>
2024-10-07 16:14:50 +0800

    测试主流程完整性

--- install/deps/fight.py
+++ install/deps/fight.py
@@ -1,11 +1,10 @@
 from json import load,loads
-from time import sleep,time,localtime,strftime
+from time import sleep,time
 from pathlib import Path
 from random import randint,shuffle
 
 from maa.context import Context
 from maa.custom_action import CustomAction
-from maa.custom_recognition import CustomRecognition
 
 from .infos import base_roi
 from .infos import common
@@ -20,12 +19,12 @@ def rec_name_list() -> list[str]:
 def rec_list() -> list:
     return []
 def act_name_list() -> list[str]:
-    return ["Fight","Move","Vision_move","Thumb_ups"] + ["Check_weekly"] + ["OS_round"]
+    return ["Fight","Move","Vision_move","Thumb_ups"] + ["OS_round"]
 def act_list() -> list:
     Move = common.Move
     Vision_move = common.Vision_move
     OS_round = Opera_Singer.OS_round
-    return [Fight(),Move(),Vision_move(),Thumb_ups()] + [Check_weekly()] + [OS_round()]
+    return [Fight(),Move(),Vision_move(),Thumb_ups()] + [OS_round()]
 
 def get_roi_base_on_state(roi_state:str):
     match roi_state:
@@ -34,13 +33,12 @@ def get_roi_base_on_state(roi_state:str):
         case "Android16_9":
             roi = base_roi.Android16_9
         case _:
-            raise("roi 声明参数异常，请联系开发者。")
+            raise ValueError("roi 声明参数异常，请联系开发者。")
         
     return roi
 
 class Fight(CustomAction):
     def run(self, context: Context, argv: CustomAction.RunArg) -> bool:
-        current_real_time = strftime("%H:%M:%S",localtime(time()))
         model_list = "匹配模式"
         character_list = "歌剧演员"
         with open(f"{main_path}/config/fight_config.json","r",encoding="utf-8") as f:
@@ -58,25 +56,20 @@ class Fight(CustomAction):
             stop_options = data["停止相关设置"]
             up_weekly = bool(stop_options["启用周上限限制"])
             reputation_limit = int(stop_options["最低人品值"])
-            time_limit = bool(stop_options["启用时间限制"])
-            limit_time = int or float or str(stop_options["限制时间"])
-            times_limit = bool(stop_options["启用次数限制"])
+            limit_time = int or float(stop_options["限制时间"])
             limit_times = int(stop_options["限制次数"])
             
             check_options = data["检测频率设置"]
             check_reputation_rate = check_options["检测人品值频率"]
             check_weely_rate = check_options["检测周上限频率"]
 
-            stop_dict = {"周上限限制": up_weekly, "时间限制": time_limit, "次数限制": times_limit}
+            stop_dict = {"周上限限制": up_weekly, "时间限制": limit_time, "次数限制": limit_time}
             del_list = []
-            for key ,value in stop_dict.items():
+            for key,value in list(stop_dict.items()):
                 if value == False:
                     del_list.append(key)
             for key in del_list:
                 del stop_dict[key]
-            for key,value in ["时间限制","次数限制"],[limit_time,limit_times]:
-                if key in stop_dict.keys():
-                    stop_dict[key] = value
 
         context.override_pipeline({"fight_检测人品值": {"custom_recognition_param": {"lowest" : reputation_limit}}})
         context.override_pipeline({"匹配成功":{"post_delay": 7000, "next": []}})
@@ -109,7 +102,7 @@ class Fight(CustomAction):
                         context.run_pipeline("随机视角移动")
 
                 case _:
-                    raise (f"Class Error:{__class__.__name__},please contact to the developers.")
+                    raise ValueError(f"Class Error:{__class__.__name__},please contact to the developers.")
                 
             if time_diff >= 235:
                 context.run_pipeline("fight_打开设置")
@@ -137,21 +130,16 @@ class Fight(CustomAction):
                 context.run_pipeline("fight_准备开始")
 
             else:
-                raise (f"Class Error:{__class__.__name__},please contact to the developers.")
+                raise ValueError(f"Class Error:{__class__.__name__},please contact to the developers.")
 
         def main(desktop_notice:bool=desktop_notice,email_notice:bool=email_notice,
                  model_list:list=model_list,model_list_random:bool=model_list_random,
                  character_list:list=character_list,character_list_random:bool=character_list_random,
                  thumbs_up:bool=thumbs_up,
                  reputation_limit:int=reputation_limit,up_weekly:bool=up_weekly,
-                 time_limit:bool=time_limit,limit_time:int|float=limit_time,
-                 times_limit:bool=times_limit,limit_times:int=limit_times,
+                 limit_time:int|float=limit_time,limit_times:int=limit_times,
+                 check_reputation_rate = check_reputation_rate , check_weely_rate = check_weely_rate
                  ) -> None:
-
-
-            character_list_len = len(character_list)
-            if character_list_len == 1:
-                pass
 
             def list_ramdon(m_list_random:bool=model_list_random,c_list_random:bool=character_list_random): #模式、角色队列乱序
                 if m_list_random == True:
@@ -164,18 +152,73 @@ class Fight(CustomAction):
             else:
                 context.override_pipeline({"fight_点赞": {"custom_action_param": {"model": model}}})
 
+            stop_keys = list(stop_dict.keys())
+            if "时间限制" in stop_keys:
+                current_time = int(time())
+                limit_time = int or float(stop_dict["时间限制"])
+                if type(limit_time) == int:
+                    stop_time = current_time + limit_time*60
+                if type(limit_time) == float:
+                    limit_time = round(limit_time,1)
+                    stop_time = current_time + limit_time*60*60
+                limit_time = True
 
-            while True:
+            if "次数限制" in stop_keys:
+                limit_time = int(stop_dict["次数限制"])
+            else:
+                limit_times = int(-1)
+            
+            weekly = bool(True)
+            limit_time = bool(limit_time)
+
+            real_time = int(time())
+            fight_times_weekly = int(0)
+            fight_times_reputation = int(0)
+
+            while weekly and limit_time and limit_times:
                 list_ramdon()
+                real_time = int(time())
                 for character in character_list:
+                    if real_time >= stop_time:
+                        limit_time = False
+                        break
+                    if weekly == False or limit_time == False or limit_times == False:
+                        break
+
                     for model in model_list:
+                        if weekly == False or limit_times == False:
+                            break
+                        if real_time >= stop_time:
+                            limit_time = False
+                            break
+
                         ready(model,character)
                         if model == "匹配模式" or model == "排位模式":
                             fight_main(character)
                         elif model == "捉迷藏":
                             pass
                         else:
-                            raise (f"Class Error:{__class__.__name__},please contact to the developers.")
+                            raise ValueError(f"Class Error:{__class__.__name__},please contact to the developers.")
+
+                        real_time = int(time())
+                        fight_times_weekly += 1
+                        fight_times_reputation += 1
+                        limit_times -= 1
+                        
+                        if up_weekly == True and fight_times_weekly == check_weely_rate:
+                            context.run_pipeline("fight_检测周上限_打开骰子")
+                            weekly_statu = context.run_recognition("fight_检测周上限",context.tasker.controller.cached_image)
+                            if weekly_statu == "42000" or weekly_statu =="50400":
+                                weekly = False
+                                break
+                            fight_times_weekly = int(0)
+                            
+                        if reputation_limit != False and fight_times_reputation == check_reputation_rate:
+                            context.run_pipeline("fight_检测人品值_打开个人名片")
+                            reputation_statu = context.run_recognition("fight_检测人品值",context.tasker.controller.cached_image)
+                            if reputation_statu <= reputation_limit:
+                                break
+                            fight_times_reputation = int(0)
 
         main()
         
@@ -198,7 +241,7 @@ class Thumb_ups(CustomAction):
                     case 4:
                         context.override_pipeline({f"{model}点赞":{"roi": [1075,490,45,45]}})
                     case _:
-                        raise (f"Class Error:{__class__.__name__},please contact to the developers.")
+                        raise ValueError(f"Class Error:{__class__.__name__},please contact to the developers.")
                 context.run_pipeline(f"{model}点赞")   
         elif model == "捉迷藏":
             gamer_list = list[1,2]
@@ -210,39 +253,11 @@ class Thumb_ups(CustomAction):
                     case 2:
                         pass
                     case _:
-                        raise (f"Class Error:{__class__.__name__},please contact to the developers.")
+                        raise ValueError(f"Class Error:{__class__.__name__},please contact to the developers.")
         else:
-            raise (f"Class Error:{__class__.__name__},please contact to the developers.")
+            raise ValueError(f"Class Error:{__class__.__name__},please contact to the developers.")
 
         return True
-
-class Check_reputation(CustomRecognition):
-    def analyze(self, context: Context, argv: CustomRecognition.AnalyzeArg) -> CustomRecognition.AnalyzeResult:
-        roi_state = loads(argv.custom_recognition_param)["roi_state"]
-        roi = get_roi_base_on_state(roi_state)
-        
-        lowest = loads(argv.custom_recognition_param)["lowest"]
-
-        Get_reputation_pipe = {"Get_reputation":{
-            "timeout": 3000,
-            "recognition": "OCR",
-            "roi": roi.roi_getreputation,
-            "only_rec": True
-            }}
-        
-        rec_result = context.run_recognition("Get_reputation",argv.image,pipeline_override = Get_reputation_pipe)
-        rec_result = rec_result.best_result.text
-
-        if rec_result <= lowest:
-            context.override_pipeline({"fight_检测人品值": {"next":["fight_人品值低_桌面提醒"]}})
-
-        return super().analyze(context, argv)
-    
-class Check_weekly(CustomRecognition):
-    def analyze(self, context: Context, argv: CustomRecognition.AnalyzeArg) -> None:
-        pass
-        return None
-    
 
 class Fight_Config_Check(CustomAction):
     def run(self, context: Context, argv: CustomAction.RunArg) -> bool:
